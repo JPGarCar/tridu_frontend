d2f52e5759ce6a6a219fc9e7f2ddeb9d3d25cea7
JP Garcia <juanpablo.garcia@alumni.ubc.ca>
2023-12-15 17:22:18 -0800

    Added basic app page with other utilities and routing.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
+import {Route, createBrowserRouter, createRoutesFromElements, RouterProvider} from "react-router-dom";
+import React from "react";
+import {CssBaseline} from "@mui/material";
 
-function App() {
 
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path="/">
+        </Route>
+    )
+)
+
+const App: React.FC = () => {
   return (
-    <>
-
-    </>
+      <>
+          <CssBaseline />
+          <RouterProvider router={router} />
+      </>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import {DevSupport} from "@react-buddy/ide-toolbox";
+import {ComponentPreviews, useInitial} from "./dev";
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <DevSupport ComponentPreviews={ComponentPreviews}
+                    useInitialHook={useInitial}
+        >
+            <App/>
+        </DevSupport>
+    </React.StrictMode>,
 )
